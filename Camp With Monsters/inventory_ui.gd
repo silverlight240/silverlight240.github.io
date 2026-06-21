ffceff58221ca7868290c5a2ba8072226482c843
silverlight240 <rionsowens@gmail.com>
2026-06-21 14:01:46 -0600

    i added stuff

--- Camp With Monsters/inventory_ui.gd
+++ Camp With Monsters/inventory_ui.gd
@@ -1,5 +1,6 @@
 extends Control
 var opened = false
+var helditem: Item = null
 @export var inventory: Inventory
 func _ready() -> void:
 	close()
@@ -19,46 +20,31 @@ func close():
 	opened = false
 func _on_button_pressed(button: TextureButton):
 	var item = button.get_meta("item")
-	print(item.name)
 	if item.name == "Axe":
 		get_parent().item = "axe"
 	if item.name == "Wood Sword":
 		get_parent().item = "sword"
 	if item.name == "Wood Pickaxe":
 		get_parent().item = "pickaxe"
-
+	if item.name == "Furnace":
+		get_parent().item = "furnace"
 func _on_timer_timeout() -> void:
-	if is_instance_valid($Panel/GridContainer.get_child(0)):
-		for i in $Panel/GridContainer.get_children():
-			if is_instance_valid(i.get_child(0)):
-				i.get_child(0).queue_free()
-	if is_instance_valid($NinePatchRect/GridContainer.get_child(0)):
-		for i in $NinePatchRect/GridContainer.get_children():
-			if is_instance_valid(i.get_child(0)):
-				i.get_child(0).queue_free()
+	for i in $NinePatchRect/GridContainer.get_children():
+		if i.get_child(0) != null:
+			i.get_child(0).queue_free()
 	var id = 0
 	for i in inventory.items:
 		if i != null:
 			if i.amount > 0:
-				var spawn = TextureRect.new()
-				spawn.texture = i.texture
-				spawn.position = Vector2(25,25)
-				$Panel/GridContainer.get_child(id).add_child(spawn)
-				spawn.add_child(Label.new())
-				spawn.get_child(0).text = str(i.amount)
-				id += 1
-	id = 0
-	for i in inventory.items:
-		if i != null:
-			if i.amount > 0:
-				if id < 4:
-					var spawn = TextureButton.new()
-					spawn.texture_normal = i.texture
-					spawn.position = Vector2(25,25)
-					$NinePatchRect/GridContainer.get_child(id).add_child(spawn)
-					spawn.name = i.name
-					spawn.pressed.connect(_on_button_pressed.bind(spawn))
-					spawn.set_meta("item", i)
-					spawn.add_child(Label.new())
-					spawn.get_child(0).text = str(i.amount)
-					id += 1
+					if id < 4:
+						if i != helditem:
+							var spawn = TextureButton.new()
+							spawn.texture_normal = i.texture
+							spawn.position = Vector2(25,25)
+							$NinePatchRect/GridContainer.get_child(id).add_child(spawn)
+							spawn.name = i.name
+							spawn.pressed.connect(_on_button_pressed.bind(spawn))
+							spawn.set_meta("item", i)
+							spawn.add_child(Label.new())
+							spawn.get_child(0).text = str(i.amount)
+		id += 1
