5603fc1e907ebc2c428bea24607216960180e406
silverlight240 <rionsowens@gmail.com>
2026-06-21 13:49:14 -0600

    i fixed camp with monsters

--- Camp With Monsters/cave.gd
+++ Camp With Monsters/cave.gd
@@ -1,9 +1,11 @@
 extends Area2D
-
+#
+@onready var root = get_parent().get_parent()
 @onready var cave = $"../../Cave"
 @onready var surface = get_parent()
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.reparent(cave, true)
-		surface.hide()
-		cave.show()
+		if not cave.get_parent():
+			root.add_child(cave)
+		root.remove_child(surface)
