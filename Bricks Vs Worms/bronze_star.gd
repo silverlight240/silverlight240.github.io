1a7d38d05ad7fe09b9c0c3c30db4bdd9b1e5fa3e
silverlight240 <rionsowens@gmail.com>
2026-07-10 13:23:53 -0600

    i fixed the star rating system

--- Bricks Vs Worms/bronze_star.gd
+++ Bricks Vs Worms/bronze_star.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Bricks Vs Worms/gold_star.gd
+++ Bricks Vs Worms/gold_star.gd
@@ -2,10 +2,10 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if get_tree().get_first_node_in_group("player").points > 39:
+	if get_tree().get_first_node_in_group("player").points >= 39:
 		show()
